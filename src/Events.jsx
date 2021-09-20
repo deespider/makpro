b7dcf6c3765226e2c857e798afb7d09c009c2c36
Deespider <diptendu.biswas143@gmail.com>
2021-09-20 05:55:09 +0530

    Events-1

--- src/Events.jsx
+++ src/Events.jsx
@@ -1,11 +1,114 @@
 import React from "react";
+import "./event.scss";
 
-let Events =() =>{
-    return(
-        <>
-            <h1> Welcome to our Events page</h1>
-        </>
-    );
+let Events = () => {
+  return (
+    <>
+      <div className="row ">
+        <div className="col-4">
+          <div className="event-menu">
+            <ul>
+              <li>
+                <a href="#">Workshops</a>
+              </li>
+              <li>
+                <a href="#">Tech Talks</a>
+              </li>
+              <li>
+                <a href="#">Career Guidance</a>
+              </li>
+              <li>
+                <a href="#">Null</a>
+              </li>
+              <li>
+                <a href="#">Null</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-8">
+          <div className="">
+            <div className="recent">
+              <h3>Recently Added</h3>
+              <div className="vid_recent">
+                <iframe
+                  width="270"
+                  height="150"
+                  src="https://www.youtube.com/embed/UAxeP4Kca78?controls=0"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+                <iframe
+                  width="270"
+                  height="150"
+                  src="https://www.youtube.com/embed/fqKhAeMMdvg"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+                <iframe
+                  width="270"
+                  height="150"
+                  src="https://www.youtube.com/embed/Jph_dc7Uomw"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="evnt">
+        <div className="evnt1">
+          <div className="list1">
+            <div className="thumb" id="vdoo" onClick={vdo}>
+              <iframe
+                src="https://www.youtube.com/embed/UAxeP4Kca78?controls=0"
+                title="YouTube video player"
+                frameborder="0"
+                id="vda"
+              ></iframe>
+            </div>
+            <div className="titl">this is the title of this video.</div>
+          </div>
+          <div className="list1">
+            <div className="thumb">
+              <iframe
+                src="https://www.youtube.com/embed/UAxeP4Kca78?controls=0"
+                title="YouTube video player"
+                frameborder="0"
+              ></iframe>
+            </div>
+            <div className="titl">this is the title of this video.</div>
+          </div>
+          <div className="list1">
+            <div className="thumb">
+              <iframe
+                src="https://www.youtube.com/embed/UAxeP4Kca78?controls=0"
+                title="YouTube video player"
+                frameborder="0"
+              ></iframe>
+            </div>
+            <div className="titl">this is the title of this video.</div>
+          </div>
+        </div>
+        <div className="evnt2">
+          <div id="vdo"></div>
+        </div>
+      </div> */}
+    </>
+  );
 };
 
 export default Events;
+let a = document.getElementById("vda");
+
+let vdo = () => {
+  return (document.getElementById("vdo").innerHTML = a);
+};
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,41 +1,94 @@
 import React from "react";
-import './Nav.scss';
+import "./Nav.scss";
 import { Link, NavLink } from "react-router-dom";
-import logo from './flogo.png';
-import { FaFacebook, FaLinkedin, FaYoutube } from 'react-icons/fa';
+import logo from "./flogo.png";
+import { FaFacebook, FaLinkedin, FaYoutube } from "react-icons/fa";
 
 let Nav = () => {
   return (
     <>
-    <div className='lebudiv'>
-      <div>
-        <FaFacebook className='fafa' /><a href='https://www.facebook.com/Makaut-183906109032533' target='blank'>Facebook</a>
-        <FaLinkedin className='fafa' /><a href='https://www.linkedin.com/company/makaut-wb/' target='blank'>Linkdin</a>
-        <FaYoutube className='fafa' /><a href='https://www.youtube.com/channel/UCQhp1Nvxj4r0Ehkuoc7X-zQ' target='blank'>YouTube</a>
+      <div className="lebudiv">
+        <div>
+          <FaFacebook className="fafa" />
+          <a
+            href="https://www.facebook.com/Makaut-183906109032533"
+            target="blank"
+          >
+            Facebook
+          </a>
+          <FaLinkedin className="fafa" />
+          <a href="https://www.linkedin.com/company/makaut-wb/" target="blank">
+            Linkdin
+          </a>
+          <FaYoutube className="fafa" />
+          <a
+            href="https://www.youtube.com/channel/UCQhp1Nvxj4r0Ehkuoc7X-zQ"
+            target="blank"
+          >
+            YouTube
+          </a>
+        </div>
+        <div>Welcom To Makaut Industry Connect</div>
       </div>
-      <div>Welcom To Makaut Industry Connect</div>
-    </div>
-    <header>
-      <div className='logo'>
-        <Link className="logolink " to="/"><img src={logo} alt='logo' /></Link></div>
-      <div className='menu'>
-        <ul className='navul'>
-          <li className='lili'><NavLink activeClassName="activeitem" className='menuitem' to='/' exact={true}>Home</NavLink></li>
-          <li className='lili'><NavLink activeClassName="activeitem" className='menuitem' to='/About' exact={true}>About</NavLink></li>
-          <li className='lili' id='lili1'>EVENTS
-            <div>
-              <li><Link className='drpbtnlnk'  to='/Techtalk' exact={true}>TechTalks</Link></li>
-              <li><Link className='drpbtnlnk' to='/Webinar' exact={true}>Webinars</Link></li>
-            </div>
-          </li>
-          <li className='lili'><NavLink activeClassName="activeitem" className='menuitem' to='/Services' exact={true}>Services</NavLink></li>
-        </ul>
-      </div>
-      <div className='nav_button'>
-        <button><Link className='contact' to='/Contact'>Contact</Link></button>
-      </div>
-    </header>
-  </>
+      <header>
+        <div className="logo">
+          <Link className="logolink " to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="menu">
+          <ul className="navul">
+            <li className="lili">
+              <NavLink
+                activeClassName="activeitem"
+                className="menuitem"
+                to="/"
+                exact={true}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="lili">
+              <NavLink
+                activeClassName="activeitem"
+                className="menuitem"
+                to="/About"
+                exact={true}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="lili">
+              <NavLink
+                activeClassName="activeitem"
+                className="menuitem"
+                to="/Events"
+                exact={true}
+              >
+                Events
+              </NavLink>
+            </li>
+            <li className="lili">
+              <NavLink
+                activeClassName="activeitem"
+                className="menuitem"
+                to="/Services"
+                exact={true}
+              >
+                Services
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="nav_button">
+          <button>
+            <Link className="contact" to="/Contact">
+              Contact
+            </Link>
+          </button>
+        </div>
+      </header>
+    </>
   );
 };
 
